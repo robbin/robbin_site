138078d41e1fa4d49853f3cdadff637231668a22
robbin <fankai@gmail.com>
2013-01-15 11:37:17 +0800

    fix blog cascade save

--- models/blog.rb
+++ models/blog.rb
@@ -2,7 +2,7 @@ class Blog < ActiveRecord::Base
   acts_as_cached
   acts_as_taggable
   belongs_to :blog_content, :dependent => :destroy 
-  validates :title, :blog_content_id, :presence => true
+  validates :title, :presence => true
   validates :title, :length => {:in => 3..50}
   delegate :content, :to => :blog_content, :allow_nil => true
   
