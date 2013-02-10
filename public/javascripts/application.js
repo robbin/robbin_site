b8b7447a1d6b346eba7d1826d23c22b0e5a68b49
robbin <fankai@gmail.com>
2013-02-10 21:24:24 +0800

    insert image to editor

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,3 +4,29 @@ $(function(){
     $('div.box').hide();
   });
 });
+
+(function($){ 
+  $.fn.extend({
+    insertAtCaret: function(myValue) {
+      var $t=$(this)[0];
+      if (document.selection) {
+        this.focus();
+        sel = document.selection.createRange();
+        sel.text = myValue;
+        this.focus();
+      } else if ($t.selectionStart || $t.selectionStart == '0') {
+          var startPos = $t.selectionStart;
+          var endPos = $t.selectionEnd;
+          var scrollTop = $t.scrollTop;
+          $t.value = $t.value.substring(0, startPos) + myValue + $t.value.substring(endPos, $t.value.length);
+          this.focus();
+          $t.selectionStart = startPos + myValue.length;
+          $t.selectionEnd = startPos + myValue.length;
+          $t.scrollTop = scrollTop;
+      } else {
+        this.value += myValue;
+        this.focus();
+      }
+    }
+  })
+})(jQuery);
