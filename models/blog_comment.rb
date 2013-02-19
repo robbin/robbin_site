d101d757ff30c3bb4d89347f3b3d6769f64cc577
robbin <fankai@gmail.com>
2013-02-19 16:35:49 +0800

    add model validation

--- models/blog_comment.rb
+++ models/blog_comment.rb
@@ -1,5 +1,8 @@
 class BlogComment < ActiveRecord::Base
   acts_as_cached
+  
+  validates_presence_of     :content
+  
   belongs_to :blog, :counter_cache => :comments_count
   belongs_to :account, :counter_cache => :comments_count
 
